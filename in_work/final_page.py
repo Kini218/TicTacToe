cc8ff86b24f7cf068dc15e945750dafc5772561e
Nikita Iakovlev <nikitayakovlev548@gmail.com>
2024-05-18 13:26:22 +0300

    Поместил функционал в класс

--- in_work/final_page.py
+++ in_work/final_page.py
@@ -2,30 +2,48 @@ import tkinter as tk
 from tkinter import ttk
 import webbrowser
 
-def open_github(event):
-    webbrowser.open_new("https://github.com/Kini218")
+class FinalPage:
+    """
+    Финальная страница с благодарностью и ссылкой на git
+    """
 
-root = tk.Tk()  # Создаем корневой объект - окно
-root.title("Крестики нолики")  # Устанавливаем заголовок окна
-root.geometry("600x600+350+50")  # Устанавливаем размеры окна
+    def __init__(self, root):
+        """
+        Cоздаем корневой объект - окно
+        Устанавливаем заголовок окна
+        Устанавливаем размеры окна
+        """
+        self.root = root
+        self.root.title("Крестики нолики")
+        self.root.geometry("600x600+350+50")
 
-# Создаем холст
-canvas = tk.Canvas(root)
-canvas.pack()
+        self.setup_ui()
 
-# Cоздаем фрейм, в котором будем располагать все элементы
-frame = tk.Frame(root)
-frame.place(relwidth=1, relheight=1)
+    def setup_ui(self):
+        """
+        Создаем холст
+        Рисуем элементы
+        """
+        canvas = tk.Canvas(self.root)
+        canvas.pack()
 
-thanks_message = ttk.Label(frame, text="Спасибо за игру!", justify="center", font=("Arial", 30, "bold"))
-thanks_message.place(relx=0.5, rely=0.20, anchor="center")
+        frame = tk.Frame(self.root)
+        frame.place(relwidth=1, relheight=1)
 
-developer = ttk.Label(frame, text="Developed by Nikita", justify="center", font=("Arial", 20))
-developer.place(relx=0.5, rely=0.70, anchor="center")
+        thanks_message = ttk.Label(frame, text="Спасибо за игру!", justify="center", font=("Arial", 30, "bold"))
+        thanks_message.place(relx=0.5, rely=0.20, anchor="center")
 
-git_link = ttk.Label(frame, text="GitHub(https://github.com/Kini218)", justify="center", font=("Arial", 20), foreground="blue")
-git_link.place(relx=0.5, rely=0.80, anchor="center")
-git_link.bind("<Button-1>", open_github)
+        developer = ttk.Label(frame, text="Developed by Nikita", justify="center", font=("Arial", 20))
+        developer.place(relx=0.5, rely=0.70, anchor="center")
 
-#Запускаем отрисовку интерфейса
-root.mainloop()
+        git_link = ttk.Label(frame, text="GitHub(https://github.com/Kini218)", justify="center", font=("Arial", 20), foreground="blue")
+        git_link.place(relx=0.5, rely=0.80, anchor="center")
+        git_link.bind("<Button-1>", self.open_github)
+
+    def open_github(self, event):
+        """
+        Открываем github по клику на ссылку
+        После открытия уничтожаем страницу
+        """
+        webbrowser.open_new("https://github.com/Kini218")
+        self.root.destroy()
